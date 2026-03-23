1ec2ff66c0169f882668ef90c3ddf25d9cf0756c
vansh1811 <s.vansh1811@gmail.com>
2026-03-23 17:05:27 +0530

    fix: use env var for Google OAuth callback URL

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback", // hardcoded for now
+    callbackURL: process.env.GOOGLE_REDIRECT_URI,
   },
     async (accessToken, refreshToken, profile, done) => {
       try {
